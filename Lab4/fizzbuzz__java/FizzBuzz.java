5a33bed0ba1019d9a718cd244afdd5544355669c
Youssef Chouay <ychou031@uottawa.ca>
2022-06-20 23:52:20 -0400

    refactored all the code

--- Lab4/fizzbuzz__java/FizzBuzz.java
+++ Lab4/fizzbuzz__java/FizzBuzz.java
@@ -19,10 +19,10 @@ public class FizzBuzz {
     }
 
     public String fizzbuzz(int n){
-        if(n%3 == 0 && n%5 == 0){
-            return "FizzBuzz";
-        }
         if(n%3 == 0){
+            if(n%5 == 0){
+                return "FizzBuzz";
+            }
             return "Fizz";
         }
         if(n%5 == 0){
